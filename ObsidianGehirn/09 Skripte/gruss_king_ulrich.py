84ab4ed18a0ca232d2f58c8aef4412d562d1e420
dnbk72 <dnbk72@users.noreply.github.com>
2026-08-25 07:46:40 +0000

    Revert "Skript für Grußzeile King Ulrich hinzugefügt"
    This reverts commit a43fb5094891d332887c453dfaf608a5ac0a0dd0.

--- ObsidianGehirn/09 Skripte/gruss_king_ulrich.py
+++
@@ -1,7 +0,0 @@
-def gruss_king_ulrich():
-    """Gibt die Grußzeile für König Ulrich auf der Kommandozeile aus."""
-    print("👑 Sei gegrüßt, König Ulrich – möge dein Tag so reibungslos laufen wie ein frisch aufgesetzter Node-RED-Flow!")
-
-
-if __name__ == "__main__":
-    gruss_king_ulrich()
