a43fb5094891d332887c453dfaf608a5ac0a0dd0
dnbk72 <dnbk72@users.noreply.github.com>
2026-08-25 07:37:52 +0000

    Skript für Grußzeile King Ulrich hinzugefügt

---
+++ ObsidianGehirn/09 Skripte/gruss_king_ulrich.py
@@ -0,0 +1,7 @@
+def gruss_king_ulrich():
+    """Gibt die Grußzeile für König Ulrich auf der Kommandozeile aus."""
+    print("👑 Sei gegrüßt, König Ulrich – möge dein Tag so reibungslos laufen wie ein frisch aufgesetzter Node-RED-Flow!")
+
+
+if __name__ == "__main__":
+    gruss_king_ulrich()
